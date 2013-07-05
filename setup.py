02dbdc4df18adcf485076af5efa61448f19f90c3
Maurits van Rees <maurits@vanrees.org>
2013-07-05 19:07:30 +0200

    Note move to github in history.

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ version = '1.4dev'
 setup(name='zest.pocompile',
       version=version,
       description="Compile po files when releasing a package",
-      long_description=open("README.txt").read() + "\n" +
-                       open("CHANGES.txt").read(),
+      long_description=(open("README.txt").read() + "\n" +
+                        open("CHANGES.txt").read()),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Buildout",
-        "Topic :: Software Development :: Internationalization",
-        ],
+          "Programming Language :: Python",
+          "Framework :: Buildout",
+          "Topic :: Software Development :: Internationalization",
+          ],
       keywords='i18n locales po compile release',
       author='Maurits van Rees',
       author_email='m.van.rees@zestsoftware.nl',
-      url='http://svn.plone.org/svn/collective/zest.pocompile',
+      url='https://github.com/zestsoftware/zest.pocompile',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['zest'],
